2d890ba5b54690a787780446bc4e85ae315a350a
Daniel Nakajima <nakajimadaniel96@gmail.com>
2021-04-30 19:36:29 -0300

    modalAddFood component refactor to function

--- src/components/ModalAddFood/index.jsx
+++ src/components/ModalAddFood/index.jsx
@@ -1,30 +1,23 @@
-import { Component, createRef } from 'react';
+import { createRef } from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
 import Modal from '../Modal';
 import Input from '../Input';
 
-class ModalAddFood extends Component {
-  constructor(props) {
-    super(props);
 
-    this.formRef = createRef();
-  }
+export default function ModalAddFood({ setIsOpen, isOpen, handleAddFood }) {
 
-  handleSubmit = async data => {
-    const { setIsOpen, handleAddFood } = this.props;
+  const formRef = createRef();
 
+  async function handleSubmit(data) {
     handleAddFood(data);
     setIsOpen();
-  };
+  }
 
-  render() {
-    const { isOpen, setIsOpen } = this.props;
-
-    return (
+  return (
       <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form ref={this.formRef} onSubmit={this.handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Novo Prato</h1>
           <Input name="image" placeholder="Cole o link aqui" />
 
@@ -39,9 +32,6 @@ class ModalAddFood extends Component {
             </div>
           </button>
         </Form>
-      </Modal>
-    );
-  }
-};
-
-export default ModalAddFood;
+    </Modal>
+  )
+}
